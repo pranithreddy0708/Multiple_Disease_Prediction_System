965b76b55d349628106ea403a4959bbf3190f7e9
chotu <pranithreddymanthena@gmail.com>
2026-07-31 00:04:21 +0530

    Enable debug mode for template auto-reloading

--- app.py
+++ app.py
@@ -392,4 +392,4 @@ def predict_kidney():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", port=5100, debug=False)
+    app.run(host="0.0.0.0", port=5100, debug=True)
